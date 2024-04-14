005716027191de86eeabf7c690670a5f839e0dc5
Pavlov Danil <danpavkzn@mail.ru>
2024-04-14 20:39:27 +0300

    complete 1.7

---
+++ sitewomen/women/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, re_path, register_converter
+from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
+
+urlpatterns = [
+    path('', views.index),  # # http://127.0.0.1:8000
+    path('cats/<int:cat_id>/', views.categories),   # http://127.0.0.1:8000/cats/2/
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),   # http://127.0.0.1:8000/cats/dsadad/
+    path('archive/<year4:year>/', views.archive),
+]
---
+++ sitewomen/women/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse("Страница приложения women")
+
+
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    if request.POST:
+        print(request.POST)
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    if year > 2024:
+        raise Http404()
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{year}</p>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound(f"<h1>Страница не найдена</h1>")
