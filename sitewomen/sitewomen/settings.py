f14f976b2cff8c8f0f77f59906b67ba48c16821e
Pavlov Danil <danpavkzn@mail.ru>
2024-07-07 04:09:10 +0300

    3.4 unit

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django_extensions',
     'women.apps.WomenConfig',
 ]
 
