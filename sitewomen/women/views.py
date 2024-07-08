8f937ed1fd87ee80c2378a259f53596b0d208807
Pavlov Danil <danpavkzn@mail.ru>
2024-07-08 16:36:10 +0300

    3.6

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,16 +1,16 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
+from .models import Women
 
-menu = [{'title':'О сайте', 'url_name': 'about'},
-        {'title':'Добавить статью', 'url_name': 'add_page'},
-        {'title':'Обратная связь', 'url_name': 'contact'},
-        {'title':'Войти', 'url_name': 'login'}
-]
-
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}
+        ]
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
@@ -28,10 +28,11 @@ cats_db = [
 
 
 def index(request):
+    posts = Women.objects.filter(is_published=1)
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': data_db,
+        'posts': posts,
         'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=data)
@@ -40,17 +41,30 @@ def index(request):
 def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+    return render(request, 'women/post.html', data)
+
 
 def addpage(request):
     return HttpResponse("Добавление статьи")
 
+
 def contact(request):
     return HttpResponse("Обратная связь")
 
+
 def login(request):
     return HttpResponse("Авторизация")
+
 
 def show_category(request, cat_id):
     data = {
@@ -60,6 +74,7 @@ def show_category(request, cat_id):
         'cat_selected': cat_id,
     }
     return render(request, 'women/index.html', context=data)
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("Страница не найдена")
