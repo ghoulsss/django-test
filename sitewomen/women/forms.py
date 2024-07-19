abffd6f4405d8f6e6155ff32b443b305c9e62dc1
Pavlov Danil <danpavkzn@mail.ru>
2024-07-19 17:05:47 +0300

    7.4 unit

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -1,14 +1,29 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, MaxLengthValidator, BaseValidator
+from django.utils.deconstruct import deconstructible
+
 from .models import Category, Husband
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок',
-                            widget=forms.TextInput(attrs={'class': 'form-input'}))
-    slug = forms.SlugField(max_length=255, required=False, label='URL')
+    title = forms.CharField(max_length=255, label='Заголовок', min_length=5,
+                            widget=forms.TextInput(attrs={'class': 'form-input'}),
+                            error_messages={'min_length': 'Слишком короткий заголовок',
+                                            'required': 'Без заголовка никак'})
+    slug = forms.SlugField(max_length=255, required=False, label='URL', validators=[
+        MinLengthValidator(5, message='Минимум 5 символов'),
+        MaxLengthValidator(100, message='Минимум 5 символов')
+    ])
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), label='Контент')
     is_published = forms.BooleanField(required=False, label='Статус', initial=True)
     cat = forms.ModelChoiceField(queryset=Category.objects.all(),
                                  label='Категории', empty_label='Категория не выбрана')
     husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False,
                                      label='Муж', empty_label='Не замужем')
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        ALLOWED_CHARS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя0123456789- '
+        if not (set(title) <= set(ALLOWED_CHARS)):
+            raise ValidationError('Должны быть только русские символы, дефис или пробел')
