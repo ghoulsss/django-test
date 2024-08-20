fb838b05c57798f960128d22d1afd2261f1c5bea
Pavlov Danil <danpavkzn@mail.ru>
2024-08-20 22:58:48 +0300

    tests made

--- sitewomen/women/tests.py
+++ sitewomen/women/tests.py
@@ -1,3 +1,27 @@
+from http import HTTPStatus
+
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class GetPagesTestCase(TestCase):
+    def setUp(self):
+        "Инициализация"
+
+    def test_mainpage(self):
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code,  HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'women/index.html')
+        self.assertEqual(response.context_data['title'], 'Главная страница')
+
+    def test_redirect_addpage(self):
+        path = reverse('add_page')
+        redirect_uri = reverse('users:login') + '?next=' + path
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, redirect_uri)
+
+    def tearDown(self):
+        "Действия после выполнения теста"
